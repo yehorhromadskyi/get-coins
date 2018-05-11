0403aa1ed73555fdc7cdc85a193bb522a32754ca
Yehor Hromadskyi <yhrom@softserveinc.com>
2018-05-11 16:51:58 +0300

    RoversViewController

---
+++ src/GetCoins.iOS/ViewControllers/RoversViewController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+
+using CoreFoundation;
+using UIKit;
+using Foundation;
+
+namespace GetCoins.iOS.ViewControllers
+{
+    [Register("RoversViewController")]
+    public class RoversViewController : UITableViewController
+    {
+        public RoversViewController()
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view
+        }
+    }
+}
