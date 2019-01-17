b2c81ea400dde39621f919e5bf43beddf76d239b
Yehor Hromadskyi <fixed-term.yehor.hromadskyi@bosch-softtec.com>
2019-01-17 21:49:13 +0100

    packages update, use under proxy

--- src/SpaceProgram.iOS/Services/HttpService.cs
+++ src/SpaceProgram.iOS/Services/HttpService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 
 namespace SpaceProgram.iOS.Services
@@ -6,7 +7,11 @@ namespace SpaceProgram.iOS.Services
     public sealed class HttpService
     {
         private static readonly Lazy<HttpClient> httpClient =
-            new Lazy<HttpClient>(() => new HttpClient());
+            new Lazy<HttpClient>(() => new HttpClient(new HttpClientHandler()
+            {
+                Proxy = new WebProxy("http://127.0.0.1:5865"),
+                UseProxy = true
+            }));
 
         public static HttpClient Client => httpClient.Value;
 
