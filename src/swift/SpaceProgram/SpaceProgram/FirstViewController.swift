4fa1f505debccc109ae1cf851f9a864c456d1819
Yehor Hromadskyi <>
2019-03-13 10:46:00 +0100

    new iOS-swift project

---
+++ src/swift/SpaceProgram/SpaceProgram/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SpaceProgram
+//
+//  Created by Yehor Hromadskyi on 13.03.19.
+//  Copyright © 2019 Yehor Hromadskyi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ src/swift/SpaceProgram/SpaceProgram/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SpaceProgram
+//
+//  Created by Yehor Hromadskyi on 13.03.19.
+//  Copyright © 2019 Yehor Hromadskyi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
